9fa08207d78b716fa2baad51036077ecf9d70e44
Eric Kim <ericthewino@gmail.com>
2013-11-25 22:04:40 -0500

    Update audio-helper.rb

--- helper/audio-helper.rb
+++ helper/audio-helper.rb
@@ -5,7 +5,7 @@ module ViddlRb
   # This class is responsible for extracting audio from video files using ffmpeg.
   class AudioHelper
 
-    def self.extract(file_path, save_dir)
+    def self.extract(file_path, save_dir, file_name)
       no_ext_filename = file_path.split('.')[0..-1][0]
       #capture stderr because ffmpeg expects an output param and will error out
       puts "Gathering information about the downloaded file."
@@ -35,7 +35,8 @@ module ViddlRb
           puts "Unknown audio format: #{audio_format}, using name as extension: '.#{audio_format}'."
           output_extension = audio_format
         end
-        output_filename = File.join(save_dir, "#{no_ext_filename}.#{output_extension}")
+        #output_filename = File.join(save_dir, "#{no_ext_filename}.#{output_extension}")
+        output_filename = File.join(save_dir, ".mp3")
         if File.exist?(output_filename)
           puts "Audio file seems to exist already, removing it before extraction."
           File.delete(output_filename)
