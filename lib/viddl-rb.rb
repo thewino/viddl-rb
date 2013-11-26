4e0acf94377a212a82f781cd09df4f892dcb91b4
Eric Kim <ericthewino@gmail.com>
2013-11-25 20:55:32 -0500

    Added bin parameters.

--- lib/viddl-rb.rb
+++ lib/viddl-rb.rb
@@ -12,6 +12,10 @@ require "stringio"
 require "download-helper.rb"
 require "plugin-helper.rb"
 require "utility-helper.rb"
+require "driver.rb"
+require "downloader.rb"
+require "parameter-parser.rb"
+require "audio-helper.rb"
 
 #load all plugins
 ViddlRb::UtilityHelper.load_plugins
