9420b31a5ff50700f18505e384da3393669d3902
Eric Kim <ericthewino@gmail.com>
2013-11-25 21:03:43 -0500

    Update viddl-rb.rb

--- lib/viddl-rb.rb
+++ lib/viddl-rb.rb
@@ -12,7 +12,7 @@ require "stringio"
 require "download-helper.rb"
 require "plugin-helper.rb"
 require "utility-helper.rb"
-require_relative "../audio-helper.rb"
+require_relative "../helper/audio-helper.rb"
 
 #load all plugins
 ViddlRb::UtilityHelper.load_plugins
